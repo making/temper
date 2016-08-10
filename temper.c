ca44d35ed14f1870e51bf94813f725f7363215ec
Toshiaki Maki <makingx@gmail.com>
2016-08-11 04:27:41 +0900

    add server

--- temper.c
+++ temper.c
@@ -5,6 +5,10 @@
 
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include "pcsensor.h"
 
 /* Calibration adjustments */
@@ -15,44 +19,85 @@ static float offset = -0.85;
 int main(){
   int passes = 0;
   float tempc = 0.0000;
-  do {
-    usb_dev_handle* lvr_winusb = pcsensor_open();
+  int sock0;
+  struct sockaddr_in addr;
+  struct sockaddr_in client;
+  int len;
+  int sock;
+  int n;
+  int yes = 1;
+  char buf[2048];
 
-    if (!lvr_winusb) {
-      /* Open fails sometime, sleep and try again */
-      sleep(3);
-    }
-    else {
-	
-      tempc = pcsensor_get_temperature(lvr_winusb);
-      pcsensor_close(lvr_winusb);
-    }
-    ++passes;
-  }
-  /* Read fails silently with a 0.0 return, so repeat until not zero
-     or until we have read the same zero value 3 times (just in case
-     temp is really dead on zero */
-  while ((tempc > -0.0001 && tempc < 0.0001) || passes >= 4);
+  sock0 = socket(AF_INET, SOCK_STREAM, 0);
 
-  if (!((tempc > -0.0001 && tempc < 0.0001) || passes >= 4)) {
-    /* Apply calibrations */
-    tempc = (tempc * scale) + offset;
+  addr.sin_family = AF_INET;
+  addr.sin_port = htons(12345);
+  addr.sin_addr.s_addr = INADDR_ANY;
 
-    struct tm *utc;
-    time_t t;
-    t = time(NULL);
-    utc = gmtime(&t);
-		
-    char dt[80];
-    strftime(dt, 80, "%Y-%m-%dT%H:%M:%SZ", utc);
+  setsockopt(sock0, SOL_SOCKET, SO_REUSEADDR, (const char *)&yes, sizeof(yes));
 
-    printf("{\"datetime\":\"%s\", \"temper\":%f}\n", dt, tempc);
-    fflush(stdout);
-
-    return 0;
-  }
-  else {
+  if (bind(sock0, (struct sockaddr *)&addr, sizeof(addr)) != 0) {
+    perror("bind");
     return 1;
   }
 
+  if (listen(sock0, 5) != 0) {
+    perror("listen");
+    return 1;
+  }
+
+  while (1) {
+    len = sizeof(client);
+    sock = accept(sock0, (struct sockaddr *)&client, &len);
+
+    if (sock < 0) {
+      perror("accept");
+      break;
+    }
+
+    printf("accepted\n");
+
+    while (1) {
+      do {
+	usb_dev_handle* lvr_winusb = pcsensor_open();
+	if (!lvr_winusb) {
+	  /* Open fails sometime, sleep and try again */
+	  sleep(1);
+	}
+	else {
+	  tempc = pcsensor_get_temperature(lvr_winusb);
+	  printf("tmp=%f\n", tempc);
+	  pcsensor_close(lvr_winusb);
+	}
+      } while ((tempc > -0.0001 && tempc < 0.0001));
+
+      if (!(tempc > -0.0001 && tempc < 0.0001)) {
+	/* Apply calibrations */
+	tempc = (tempc * scale) + offset;
+
+	struct tm *utc;
+	time_t t;
+	t = time(NULL);
+	utc = gmtime(&t);
+		
+	char dt[80];
+	strftime(dt, 80, "%Y-%m-%dT%H:%M:%SZ", utc);
+
+	memset(buf, 0, sizeof(buf));
+	snprintf(buf, sizeof(buf),"{\"datetime\":\"%s\", \"temper\":%f}\r\n", dt, tempc);
+	fflush(stdout);
+
+	n = send(sock, buf, (int)strlen(buf), 0);
+	if (n < 0) {
+	  perror("send");
+	  break;
+	}
+      }
+      sleep(3);
+    }
+    close(sock);
+  }
+
+  close(sock0);
+  return 0;
 }
